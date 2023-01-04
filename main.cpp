4dceecfd4f62482cebd509f76bb8f66863a1bb66
sescer <a.nepein@g.nsu.ru>
2023-01-04 16:51:31 +0700

    Included SFML and openCV

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <opencv2/opencv.hpp>
+using namespace cv;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char** argv)
+{
+    if ( argc != 2 )
+    {
+        printf("usage: %s <Image_Path>\n", argv[0]);
+        return -1;
+    }
+    Mat image;
+    image = imread( argv[1], 1 );
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    window.clear();
+    window.draw(shape);
+    window.display();
+
+
+    waitKey(0);
+
+    window.close();
+
     return 0;
 }
